307eb2dba242c7600f6655e4a2e9fceecef5a7d1
d3dev <admin@d3d.info>
2019-11-28 15:25:07 +0000

    feature: chop_down_table option. issues/62

---
+++ test/testdata/issues/_issue-62_1.lua
@@ -0,0 +1,15 @@
+array = {
+    variable1, variable2, variable3, variable4, variable5, variable6, variable7,
+    variable8, variable9, variable10
+}
+
+kv_table = {
+    key1 = variable1, key2 = variable2, key3 = variable3, key4 = variable4,
+    key5 = variable5, key6 = variable6, key7 = variable7, key8 = variable8,
+    key9 = variable9, key10 = variable10
+}
+
+mixed_table = {
+    key1 = variable1, key2 = variable2, key3 = variable3, variable4, variable5,
+    variable6, variable7, variable8, variable9, variable10
+}
